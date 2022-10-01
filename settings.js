bdd9cfada3f13d842c22ddfa1cbe86b824135208
gggmanii123 <114810540+gggmanii123@users.noreply.github.com>
2022-10-01 19:20:39 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -40,9 +40,9 @@ global.alivepic = `https://i.ibb.co/Bcf9Kbf/IMG-20221001-WA0057.jpg` // alive pi
 
 global.dragonchat = "false"  // chat bot  "true" / "false" (200 auto replies in this)
 global.callblock = "false" // call block "true" / "false" ("true"  to block callers)
-global.antibad = "false" // To remove bad word senders
+global.antibad = "true" // To remove bad word senders
 global.antispam = "true" // To remove or block spammers
-global.antilink = "false" // To remove group link sharing peoples
+global.antilink = "true" // To remove group link sharing peoples
 global.upsongvoice = "true" // true/false \\ bot can upload song as voice note (it use your data and storage bacause auto download) you can put "false" (anyone can't get songs as voice note / your data is not wasted)
 global.welcom_msg = "false" // Welcome / Goodbye   [true/false]
 
@@ -122,7 +122,7 @@ global.rkyt = []
 global.wlcm = []
 global.gcrevoke = []
 global.autorep = []
-global.ntilink = []
+global.ntilink = [true]
 
 
 

eacae370868e55296f659cfb5639fe195d70fa3f
gggmanii123 <114810540+gggmanii123@users.noreply.github.com>
2022-10-01 18:50:09 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -38,10 +38,10 @@ global.location = "India, Mizoram, Aizawl" // location
 //bot body 
 global.alivepic = `https://i.ibb.co/Bcf9Kbf/IMG-20221001-WA0057.jpg` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
 
-global.dragonchat = "true"  // chat bot  "true" / "false" (200 auto replies in this)
+global.dragonchat = "false"  // chat bot  "true" / "false" (200 auto replies in this)
 global.callblock = "false" // call block "true" / "false" ("true"  to block callers)
 global.antibad = "true" // To remove bad word senders
-global.antispam = "default" // To remove or block spammers
+global.antispam = "true" // To remove or block spammers
 global.antilink = "true" // To remove group link sharing peoples
 global.upsongvoice = "true" // true/false \\ bot can upload song as voice note (it use your data and storage bacause auto download) you can put "false" (anyone can't get songs as voice note / your data is not wasted)
 global.welcom_msg = "false" // Welcome / Goodbye   [true/false]

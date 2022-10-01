537d69d8d287c0dd64fd111607e536e509ec7b35
gggmanii123 <114810540+gggmanii123@users.noreply.github.com>
2022-10-01 18:01:46 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ global.alivemsg = `📤I am alive😎
 🤷‍♀️༺♥🇮ᵐʀᵃɴ 🇲 ᵃɴⁱ ♥﻿༻ ` //Costomize Alive Message (type your message in `` )
 
 
-global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
 global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
 global.autoReadGc = false //auto reading in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
@@ -38,13 +38,13 @@ global.location = "India, Mizoram, Aizawl" // location
 //bot body 
 global.alivepic = `https://i.ibb.co/Bcf9Kbf/IMG-20221001-WA0057.jpg` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
 
-global.dragonchat = "false"  // chat bot  "true" / "false" (200 auto replies in this)
+global.dragonchat = "true"  // chat bot  "true" / "false" (200 auto replies in this)
 global.callblock = "false" // call block "true" / "false" ("true"  to block callers)
-global.antibad = "false" // To remove bad word senders
-global.antispam = "true" // To remove or block spammers
-global.antilink = "false" // To remove group link sharing peoples
-global.upsongvoice = "false" // true/false \\ bot can upload song as voice note (it use your data and storage bacause auto download) you can put "false" (anyone can't get songs as voice note / your data is not wasted)
-global.welcom_msg = "true" // Welcome / Goodbye   [true/false]
+global.antibad = "true" // To remove bad word senders
+global.antispam = "default" // To remove or block spammers
+global.antilink = "true" // To remove group link sharing peoples
+global.upsongvoice = "true" // true/false \\ bot can upload song as voice note (it use your data and storage bacause auto download) you can put "false" (anyone can't get songs as voice note / your data is not wasted)
+global.welcom_msg = "false" // Welcome / Goodbye   [true/false]
 
 
 global.owner = ['+923110088057']
